edf0b03083da765638cdcc9f6d01893c0e525fc0
andrei-pl <andrey.pld@gmail.com>
2014-05-28 03:11:12 +0300

    Rectangular Cuboid added

---
+++ High-Quality-Classes/Cohesion-and-Coupling/RectangularCuboid.cs
@@ -0,0 +1,39 @@
+﻿namespace CohesionAndCoupling
+{
+    using System;
+
+    public static class RectangularCuboid
+    {
+        public static double Width { get; set; }
+
+        public static double Height { get; set; }
+
+        public static double Depth { get; set; }
+
+        public static double CalcVolume()
+        {
+            double volume = Width * Height * Depth;
+            return volume;
+        }
+
+        public static double CalcDiagonalXyz()
+        {
+            return GeometryUtils.CalcDistance3D(0, 0, 0, Width, Height, Depth);
+        }
+
+        public static double CalcDiagonalXY()
+        {
+            return GeometryUtils.CalcDistance2D(0, 0, Width, Height);
+        }
+
+        public static double CalcDiagonalXZ()
+        {
+            return GeometryUtils.CalcDistance2D(0, 0, Width, Depth);
+        }
+
+        public static double CalcDiagonalYZ()
+        {
+            return GeometryUtils.CalcDistance2D(0, 0, Height, Depth);
+        }
+    }
+}
